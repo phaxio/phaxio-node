7e69fd0cba3763a766c4271c83da3a008257dbc8
Jeremy Jackson <jeremy.jackson@voyant.com>
2018-08-15 16:01:14 -0600

    Bring in the apikey and apisecret to the ENV before running any tests.

--- test/before-all.test.js
+++ test/before-all.test.js
@@ -1,8 +1,7 @@
 /* eslint no-console: "off" */
 const { config } = require('dotenv');
 
-const result = config();
-
-if (result.error) throw result.error;
-
-console.log('Parsed ENV Config:', result.parsed);
+before(() => {
+  const result = config();
+  if (result.error) throw result.error;
+});
