e3937e038ec3d1fe47246bb245acec0efc6569df
Jeremy Jackson <jeremy.jackson@voyant.com>
2018-08-16 15:03:01 -0600

    Disable testing of PhoneNumber class unless explicitly required since it uses up funds.

--- test/phone-number/index.test.js
+++ test/phone-number/index.test.js
@@ -1,12 +1,14 @@
-/* eslint arrow-body-style: "off" */
+/* eslint arrow-body-style: "off", func-names: "off" */
 const expect = require('expect.js');
 
 const PhoneNumber = require('../../src/phone-number');
 
+// Running the PhoneNumber tests will use up funds. We skip it by default.
+// Change the below variable to `true` to run the tests and accept the loss of funds.
+const runPhoneNumbers = false;
+
 // For some of the tests, when run in the during peak hours, they will exceed the 2s timeout.
-// Bumping timeout up manually to 3s. You only get access to `this` when not using arrow functions,
-// which is what the eslint-disable is about.
-/* eslint-disable-next-line func-names */
+// Bumping timeout up manually to 3s.
 describe('class: PhoneNumber', function () {
   this.timeout(3000);
 
@@ -15,73 +17,75 @@ describe('class: PhoneNumber', function () {
     expect(PhoneNumber).to.be.a('function');
   });
 
-  describe('post-instantiation', () => {
-    let phoneNumber;
-    let sampleNumber;
-    before(() => {
-      phoneNumber = new PhoneNumber(process.env.TEST_APIKEY, process.env.TEST_APISECRET, 'https://api.phaxio.com/v2.1');
-    });
+  if (runPhoneNumbers) {
+    describe('post-instantiation', () => {
+      let phoneNumber;
+      let sampleNumber;
+      before(() => {
+        phoneNumber = new PhoneNumber(process.env.TEST_APIKEY, process.env.TEST_APISECRET, 'https://api.phaxio.com/v2.1');
+      });
 
-    it('should export appropriate properties and functions', () => {
-      expect(phoneNumber).to.have.property('apiKey');
-      expect(phoneNumber).to.have.property('apiSecret');
-      expect(phoneNumber).to.have.property('url');
-      expect(phoneNumber).to.have.property('releaseNumber');
-      expect(phoneNumber).to.have.property('listNumbers');
-      expect(phoneNumber).to.have.property('getNumberInfo');
-      expect(phoneNumber).to.have.property('provisionNumber');
-    });
+      it('should export appropriate properties and functions', () => {
+        expect(phoneNumber).to.have.property('apiKey');
+        expect(phoneNumber).to.have.property('apiSecret');
+        expect(phoneNumber).to.have.property('url');
+        expect(phoneNumber).to.have.property('releaseNumber');
+        expect(phoneNumber).to.have.property('listNumbers');
+        expect(phoneNumber).to.have.property('getNumberInfo');
+        expect(phoneNumber).to.have.property('provisionNumber');
+      });
 
-    describe('method: provisionNumber', () => {
-      it('should successfully provision a number with the requeted parameters', () => {
-        // { country_code: null, area_code: null, callback_url: null }
-        return phoneNumber.provisionNumber({ country_code: 1, area_code: 847 })
-          .then((response) => {
-            sampleNumber = response.data.phone_number;
-            expect(response.success).to.be.ok();
-            expect(response.message).to.be('Number provisioned successfully!');
-            expect(response.data.phone_number).to.match(/\+1847[0-9]{7}/);
-            expect(response.data.country).to.be('United States of America');
-          })
-          .catch((err) => { throw err; });
+      describe('method: provisionNumber', () => {
+        it('should successfully provision a number with the requeted parameters', () => {
+          // { country_code: null, area_code: null, callback_url: null }
+          return phoneNumber.provisionNumber({ country_code: 1, area_code: 847 })
+            .then((response) => {
+              sampleNumber = response.data.phone_number;
+              expect(response.success).to.be.ok();
+              expect(response.message).to.be('Number provisioned successfully!');
+              expect(response.data.phone_number).to.match(/\+1847[0-9]{7}/);
+              expect(response.data.country).to.be('United States of America');
+            })
+            .catch((err) => { throw err; });
+        });
+      });
+
+      describe('method: listNumbers', () => {
+        it('should get a list of numbers you have registered based on the set filters', () => {
+          return phoneNumber.listNumbers()
+            .then((response) => {
+              expect(response.success).to.be.ok();
+              expect(response.message).to.be('Retrieved user phone numbers successfully');
+              expect(response.data).to.be.an(Array);
+              expect(response.data.filter(rec => rec.phone_number === sampleNumber)).to.be.ok();
+            })
+            .catch((err) => { throw err; });
+        });
+      });
+
+      describe('method: getNumberInfo', () => {
+        it('should get information about the number requested', () => {
+          return phoneNumber.getNumberInfo(sampleNumber)
+            .then((response) => {
+              expect(response.success).to.be.ok();
+              expect(response.message).to.be('Retrieved user phone numbers successfully');
+              expect(response.data).to.be.an(Object);
+              expect(response.data.phone_number).to.be(sampleNumber);
+            })
+            .catch((err) => { throw err; });
+        });
+      });
+
+      describe('method: releaseNumber', () => {
+        it('should get a message indicating that it successfully released the number', () => {
+          return phoneNumber.releaseNumber(sampleNumber)
+            .then((response) => {
+              expect(response.success).to.be.ok();
+              expect(response.message).to.be('Number released successfully!');
+            })
+            .catch((err) => { throw err; });
+        });
       });
     });
-
-    describe('method: listNumbers', () => {
-      it('should get a list of numbers you have registered based on the set filters', () => {
-        return phoneNumber.listNumbers()
-          .then((response) => {
-            expect(response.success).to.be.ok();
-            expect(response.message).to.be('Retrieved user phone numbers successfully');
-            expect(response.data).to.be.an(Array);
-            expect(response.data.filter(rec => rec.phone_number === sampleNumber)).to.be.ok();
-          })
-          .catch((err) => { throw err; });
-      });
-    });
-
-    describe('method: getNumberInfo', () => {
-      it('should get information about the number requested', () => {
-        return phoneNumber.getNumberInfo(sampleNumber)
-          .then((response) => {
-            expect(response.success).to.be.ok();
-            expect(response.message).to.be('Retrieved user phone numbers successfully');
-            expect(response.data).to.be.an(Object);
-            expect(response.data.phone_number).to.be(sampleNumber);
-          })
-          .catch((err) => { throw err; });
-      });
-    });
-
-    describe('method: releaseNumber', () => {
-      it('should get a message indicating that it successfully released the number', () => {
-        return phoneNumber.releaseNumber(sampleNumber)
-          .then((response) => {
-            expect(response.success).to.be.ok();
-            expect(response.message).to.be('Number released successfully!');
-          })
-          .catch((err) => { throw err; });
-      });
-    });
-  });
+  }
 });
