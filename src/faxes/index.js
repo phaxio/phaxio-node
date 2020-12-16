f5f5a256b3f592a1b992b4b4473577a55fc04e56
Arit Oyekan <aaoyekanmi4@gmail.com>
2020-12-16 08:18:30 -0600

    accept file buffer

--- src/faxes/index.js
+++ src/faxes/index.js
@@ -1,8 +1,8 @@
-const fs = require('fs');
-const request = require('request-promise-native');
-const errorHandler = require('../error-handler');
+const fs = require('fs')
+const request = require('request-promise-native')
+const errorHandler = require('../error-handler')
 
-const Fax = require('./fax');
+const Fax = require('./fax')
 const {
   cancel,
   resend,
@@ -10,49 +10,49 @@ const {
   getInfo,
   getFile,
   deleteFile,
-} = require('./shared-methods');
+} = require('./shared-methods')
 
 module.exports = class {
-  constructor(apiKey, apiSecret, url, agentOptions) {
-    this.apiKey = apiKey;
-    this.apiSecret = apiSecret;
-    this.url = url;
-    this.agentOptions = agentOptions;
+  constructor (apiKey, apiSecret, url, agentOptions) {
+    this.apiKey = apiKey
+    this.apiSecret = apiSecret
+    this.url = url
+    this.agentOptions = agentOptions
 
-    this.auth = { user: this.apiKey, pass: this.apiSecret };
+    this.auth = { user: this.apiKey, pass: this.apiSecret }
   }
 
-  async cancel(id) {
-    const canc = await cancel(this.url, id, this.auth, this.agentOptions);
-    return canc;
+  async cancel (id) {
+    const canc = await cancel(this.url, id, this.auth, this.agentOptions)
+    return canc
   }
 
-  async resend(options = { id: null, callback_url: null }) {
-    const res = await resend(this.url, options.id, this.auth, options, this.agentOptions);
-    return res;
+  async resend (options = { id: null, callback_url: null }) {
+    const res = await resend(this.url, options.id, this.auth, options, this.agentOptions)
+    return res
   }
 
-  async testDelete(id) {
-    const td = await testDelete(this.url, id, this.auth, this.agentOptions);
-    return td;
+  async testDelete (id) {
+    const td = await testDelete(this.url, id, this.auth, this.agentOptions)
+    return td
   }
 
-  async getInfo(id) {
-    const gi = await getInfo(this.url, id, this.auth, this.agentOptions);
-    return gi;
+  async getInfo (id) {
+    const gi = await getInfo(this.url, id, this.auth, this.agentOptions)
+    return gi
   }
 
-  async getFile(options = { id: null, thumbnail: null }) {
-    const gf = await getFile(this.url, options.id, this.auth, options, this.agentOptions);
-    return gf;
+  async getFile (options = { id: null, thumbnail: null }) {
+    const gf = await getFile(this.url, options.id, this.auth, options, this.agentOptions)
+    return gf
   }
 
-  async deleteFile(id) {
-    const df = await deleteFile(this.url, id, this.auth, this.agentOptions);
-    return df;
+  async deleteFile (id) {
+    const df = await deleteFile(this.url, id, this.auth, this.agentOptions)
+    return df
   }
 
-  create(options = {
+  create (options = {
     to: null,
     file: null,
     content_url: null,
@@ -67,68 +67,71 @@ module.exports = class {
   }) {
     // eslint-disable-next-line consistent-return
     return new Promise((resolve, reject) => {
-      const formData = {};
+      const formData = {}
 
       Object.keys(options).forEach((rec) => {
-        if (options[rec] !== null) formData[rec] = options[rec];
-        if (typeof formData[rec] === 'boolean') formData[rec] = formData[rec].toString();
-      });
+        if (options[rec] !== null) formData[rec] = options[rec]
+        if (typeof formData[rec] === 'boolean') formData[rec] = formData[rec].toString()
+      })
 
       const req = {
         method: 'POST',
         url: `${this.url}/faxes`,
         auth: this.auth,
         agentOptions: this.agentOptions,
-      };
+      }
 
-      const caller = request(req);
-      const form = caller.form();
+      const caller = request(req)
+      const form = caller.form()
       Object.keys(formData).forEach((key) => {
         if (typeof formData[key] === 'object' && key === 'tags' && formData[key] !== null) {
           Object.keys(formData[key]).forEach((tagkey) => {
-            form.append(`tag[${tagkey}]`, formData[key][tagkey]);
-          });
+            form.append(`tag[${tagkey}]`, formData[key][tagkey])
+          })
         } else if (Array.isArray(formData[key]) && key === 'file') {
           formData[key].forEach((val) => {
-            form.append(`${key}[]`, fs.createReadStream(val));
-          });
+            form.append(`${key}[]`, fs.createReadStream(val))
+          })
         } else if (Array.isArray(formData[key])) {
           formData[key].forEach((val) => {
-            form.append(`${key}[]`, val);
-          });
+            form.append(`${key}[]`, val)
+          })
         } else if (key === 'file') {
-          form.append(key, fs.createReadStream(formData[key]));
+          //form.append(key, fs.createReadStream(formData[key]));
+          form.append(key, formData[key].buffer, {
+            filename: formData[key].filename
+          })
         } else {
-          form.append(key, formData[key]);
+          form.append(key, formData[key])
         }
-      });
+      })
 
       caller
         .then((response) => {
-          const res = JSON.parse(response);
-          if (!res.success) return reject(errorHandler(res.message));
+          const res = JSON.parse(response)
+          if (!res.success) return reject(errorHandler(res.message))
           // eslint-disable-next-line max-len
-          return resolve(new Fax(this.apiKey, this.apiSecret, this.url, res.success, res.message, res.data));
+          return resolve(new Fax(this.apiKey, this.apiSecret, this.url, res.success, res.message, res.data))
         })
-        .catch((err) => reject(err));
-    });
+        .catch((err) => reject(err))
+    })
   }
 
-  testReceive(options = {
+  testReceive (options = {
     file: null,
     from_number: null,
     to_number: null,
   }) {
     return new Promise((resolve, reject) => {
-      const formData = { direction: 'received' };
+      const formData = { direction: 'received' }
 
       Object.keys(options).forEach((rec) => {
         if (rec === 'file' && options[rec] !== null) {
-          formData[rec] = fs.createReadStream(options[rec]);
+          formData[rec] = fs.createReadStream(options[rec])
         } else if (options[rec] !== null) {
-          formData[rec] = options[rec];
+          formData[rec] = options[rec]
         }
-      });
+      })
 
       request({
         method: 'POST',
@@ -138,15 +141,15 @@ module.exports = class {
         agentOptions: this.agentOptions,
       })
         .then((response) => {
-          const res = JSON.parse(response);
-          if (!res.success) return reject(errorHandler(res.message));
-          return resolve(res);
+          const res = JSON.parse(response)
+          if (!res.success) return reject(errorHandler(res.message))
+          return resolve(res)
         })
-        .catch((err) => reject(err));
-    });
+        .catch((err) => reject(err))
+    })
   }
 
-  listFaxes(options = {
+  listFaxes (options = {
     created_before: null,
     created_after: null,
     direction: null,
@@ -157,17 +160,17 @@ module.exports = class {
     page: null,
   }) {
     return new Promise((resolve, reject) => {
-      const query = {};
+      const query = {}
       Object.keys(options).forEach((rec) => {
         if (rec === 'tags' && typeof options[rec] === 'object' && options[rec] !== null) {
           Object.keys(options[rec]).forEach((tag) => {
-            const newrec = `tag[${tag}]`;
-            query[newrec] = options[rec][tag];
-          });
+            const newrec = `tag[${tag}]`
+            query[newrec] = options[rec][tag]
+          })
         } else if (options[rec] !== null) {
-          query[rec] = options[rec];
+          query[rec] = options[rec]
         }
-      });
+      })
 
       request({
         method: 'GET',
@@ -177,11 +180,11 @@ module.exports = class {
         agentOptions: this.agentOptions,
       })
         .then((response) => {
-          const res = JSON.parse(response);
-          if (!res.success) return reject(errorHandler(res.message));
-          return resolve(res);
+          const res = JSON.parse(response)
+          if (!res.success) return reject(errorHandler(res.message))
+          return resolve(res)
         })
-        .catch((err) => reject(err));
-    });
+        .catch((err) => reject(err))
+    })
   }
-};
+}
